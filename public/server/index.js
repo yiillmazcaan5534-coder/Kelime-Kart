08f27a72875d8f4aa3c138584ef7f9ccc12d02a8
KelimeKart <kelimekart@local>
2026-08-16 02:51:33 +0300

    Fix public site homepage

--- public/server/index.js
+++ public/server/index.js
@@ -1,5 +1,13 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request)
+    let response = await env.ASSETS.fetch(request)
+
+    if (response.status === 404 && !new URL(request.url).pathname.includes('.')) {
+      const indexUrl = new URL(request.url)
+      indexUrl.pathname = '/index.html'
+      response = await env.ASSETS.fetch(new Request(indexUrl, request))
+    }
+
+    return response
   },
 }
